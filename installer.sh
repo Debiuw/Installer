561f195d621e1a97029750179ebdf4a8b6e2a954
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-08-13 20:15:24 +0300

    compatable with my lastest dot-files and debain 13

--- installer.sh
+++ installer.sh
@@ -2,23 +2,40 @@
 
 mkdir -p ~/Documents/GitHub
 git clone https://github.com/SteavenGamerYT/dot-files ~/Documents/GitHub/dot-files
-sudo apt install zsh zsh-syntax-highlighting autojump zsh-autosuggestions neofetch xclip lolcat i3 papirus-icon-theme lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings nano nemo nemo-fileroller mpv vlc gnome-mpv firefox-esr flatpak lxappearance xinput network-manager nm-applet picom policykit-1-gnome kdenlive gedit blueman flameshot curl playerctl -y
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt install zsh zsh-syntax-highlighting autojump zsh-autosuggestions neofetch xclip lolcat i3 papirus-icon-theme lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings nano nemo nemo-fileroller mpv vlc gnome-mpv firefox-esr flatpak lxappearance xinput network-manager nm-applet picom policykit-1-gnome kdenlive gedit blueman flameshot curl playerctl firefox -y
 sudo apt update
-sudo apt install brave-browser  -y
 mkdir ~/.zsh
 mkdir ~/.zsh/plugins/
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/plugins/powerlevel10k
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ~/.zsh/plugins/zsh-autocomplete
-sudo unzip ~/Documents/GitHub/dot-files/FiraCode.zip -d "/usr/share/fonts/zsh"
-sudo unzip ~/Documents/GitHub/dot-files/Meslo.zip -d "/usr/share/fonts/zsh"
-sudo fc-cache -vf
 chsh -s $(which zsh)
-rm -rf ~/.bash*
-rm -rf ~/.config
+rm -rf ~/.steavengameryt
+rm -rf ~/.bash-default
+rm -rf ~/.zshrc
+rm -rf ~/.bashrc
+rm -rf ~/.profile
+rm -rf ~/.zshenv
+rm -rf ~/.p10k.zsh
+rm -rf ~/.config/i3
+rm -rf ~/.config/picom
+rm -rf ~/.config/neofetch
+rm -rf ~/.config/polybar
+rm -rf ~/.config/kitty
+rm -rf ~/.config/BetterDiscord
+rm -rf ~/.config/hypr
+rm -rf ~/.config/waybar
+rm -rf ~/.config/starship.toml
+rm -rf ~/.config/ulauncher
+rm -rf ~/.config/yay
+rm -rf ~/.config/MangoHud
+rm -rf ~/.config/brave-flags.conf
+rm -rf ~/.ssh/config
+rm -rf ~/.config/fish
+rm -rf ~/.config/dunst
+rm -rf ~/.config/swaylock
+rm -rf ~/.config/rofi
+mkdir ~/.ssh
 mkdir ~/.config
-mdkdir ~/.ssh
 ln -s ~/Documents/GitHub/dot-files/.steavengameryt ~/.steavengameryt
 ln -s ~/Documents/GitHub/dot-files/.bash-default ~/.bash-default
 ln -s ~/Documents/GitHub/dot-files/.zshrc ~/.zshrc
@@ -41,6 +58,9 @@ ln -s ~/Documents/GitHub/dot-files/.config/MangoHud ~/.config/MangoHud
 ln -s ~/Documents/GitHub/dot-files/.config/brave-flags.conf ~/.config/brave-flags.conf
 ln -s ~/Documents/GitHub/dot-files/.ssh/config ~/.ssh/config
 ln -s ~/Documents/GitHub/dot-files/.config/fish ~/.config/fish
+ln -s ~/Documents/GitHub/dot-files/.config/dunst ~/.config/dunst
+ln -s ~/Documents/GitHub/dot-files/.config/swaylock ~/.config/swaylock
+ln -s ~/Documents/GitHub/dot-files/.config/rofi ~/.config/rofi
 wget https://github.com/odziom91/libadwaita-themes/raw/main/nordic/nordic-v42.zip
 sudo unzip nordic-v42.zip -d /root/.config/
 unzip nordic-v42.zip -d ~/.config/
